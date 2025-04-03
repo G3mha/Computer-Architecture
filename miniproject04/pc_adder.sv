cece044c359f3a5a0d91cff429528f01832b6f5c
Enricco Gemha <enriccogemha@gmail.com>
2025-04-03 11:41:33 -0400

    feat: Add instruction register module

--- miniproject04/pc_adder.sv
+++ miniproject04/pc_adder.sv
@@ -1,9 +1,8 @@
 module pc_adder(
-    input  logic [31:0] pc,         // Current PC value
-    output logic [31:0] pc_plus_4   // PC + 4 value
+    input  logic [31:0] pc,
+    output logic [31:0] pc_plus_4
 );
     
-    // Simple adder to increment PC by 4 (word alignment)
     assign pc_plus_4 = pc + 32'd4;
     
 endmodule
