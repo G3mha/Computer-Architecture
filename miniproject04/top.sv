5b3ae8456fff5ca7d61775fcc560019a9f0de652
Sailboy42 <ohimsworth@olin.edu>
2025-04-07 12:15:42 -0400

    Add ALU and Mux Modules to top
    Added alu and muxes to top file but are unfinished. Cleaned up some code to be more readable.

--- miniproject04/top.sv
+++ miniproject04/top.sv
@@ -5,7 +5,7 @@ module top (
     input  logic reset
 );
 
-// Instruction Register
+// === Instruction Register ===
 instruction_register ir (
         .clk(clk),
         .reset(reset),
@@ -14,7 +14,7 @@ instruction_register ir (
         .instruction_out(instruction)
     );
 
-// Instruction Decoder
+// === Instruction Decoder ===
 logic [3:0]  alu_op;
 logic reg_write, mem_read, mem_write, branch, jump;
 logic [1:0]  alu_src, mem_to_reg;
@@ -31,7 +31,7 @@ instruction_decoder decoder (
         .jump(jump)
     );
 
-// Immediate Generator
+// === Immediate Generator ===
 logic [31:0] imm_ext;
 
 ImmGen immgen (
@@ -54,6 +54,56 @@ pc_adder pc_incr (
     logic [31:0] pc_plus_4,
 
     .pc(pc)
+)
+
+// ALU
+alu alu_unit (
+    .a(op1),
+    .b(op2),
+    .alu_op(alu_op),
+    .result(alu_result),
+    .zero_flag(zero_flag)
+)
+// Muxes
+mux_2x1 pc_mux (
+    .in0(pc_plus_4),
+    .in1(alu_result),
+    .sel(jump),
+    .out(pc_next)
+)
+
+mux_2x1 RA_mux (
+// TODO: Implement the mux for the register address selection
+    .in0(pc),
+    .in1(alu_result),
+    .sel(jump),
+    .out(rdv_mux_out)
+)
+
+mux_2x1 op1_mux (
+// TODO: Implement the mux for the first operand selection
+    .in0(pc),
+    .in1(),
+    .sel(),
+    .out(op1_mux_out)
+)
+
+mux_2x1 op2_mux (
+// TODO: Implement the mux for the second operand selection
+    .in0(),
+    .in1(),
+    .sel(),
+    .out(op2_mux_out)
+)
+
+mux_4x1 rdv_mux (
+    //TODO: Implement the mux for the register data value selection
+    .in0(imm_ext),
+    .in1(alu_result),
+    .in2(),
+    .in3(pc_adder),
+    .sel(),
+    .out(rdv_mux_out)
 )
 
 endmodule
