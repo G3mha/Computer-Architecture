1e604b5494308d37d5f317a13361788cce051b48
Enricco Gemha <enriccogemha@gmail.com>
2025-04-14 21:49:17 -0400

    refactor: Change clunky variables on top level

--- miniproject04/top.sv
+++ miniproject04/top.sv
@@ -5,45 +5,48 @@ module top (
   input  logic reset
 );
 
-// === Program Counter ==
-logic [31:0] pc,
-logic [31:0] pc_next,
-logic [31:0] pc_plus_4,
-pc_write = 1'b1;
+// === Program Counter and Control Signals ===
+logic [31:0] pc;
+logic [31:0] pc_next;
+logic [31:0] pc_plus_4;
+logic pc_write = 1'b1;
+logic [31:0] mem_read_data;
+logic [31:0] ra_mux_out;
 
-program_counter pc (
-.clk(clk),
-.reset(reset),
-.pc_write(pc_write),
-.pc_in(pc_next),
-.pc_out(pc)
-    );
+// === Program Counter ===
+program_counter pc_unit (
+  .clk(clk),
+  .rst(reset),
+  .pc_write(pc_write),
+  .next_pc(pc_next),
+  .pc(pc)
+);
 
+// === PC Adder ===
 pc_adder pc_incr (
-    .pc(pc)
-    .imm(32'd4),
-    .pc_plus_4(pc_plus_4)
-)
+  .pc(pc),
+  .pc_plus_4(pc_plus_4)
+);
 
 // === Instruction Memory ===
 logic [31:0] instruction_mem_data;
 
 instruction_memory instruction_mem (
-    .address(pc),
-    .instruction(instruction_mem_data)
+  .address(pc),
+  .instruction(instruction_mem_data)
 );
 
 // === Instruction Register ===
 logic [31:0] instruction;
-logic ir_write = 1'b1,
+logic ir_write = 1'b1;
 
 instruction_register ir (
-        .clk(clk),
-        .reset(reset),
-        .ir_write(ir_write),
-        .instruction_in(instruction_mem_data),
-        .instruction_out(instruction)
-    );
+  .clk(clk),
+  .reset(reset),
+  .ir_write(ir_write),
+  .instruction_in(instruction_mem_data),
+  .instruction_out(instruction)
+);
 
 // === Instruction Decoder ===
 logic [3:0]  alu_op;
@@ -76,7 +79,7 @@ ImmGen immgen (
 logic [31:0] rs1_data, rs2_data;
 logic [31:0] rdv_mux_out;
 
-reg_file reg_file (
+reg_file registers (
   .clk(clk),
   .reset(reset),
   .reg_write(reg_write),
@@ -86,23 +89,6 @@ reg_file reg_file (
   .rd_data(rdv_mux_out),
   .rs1_data(rs1_data),
   .rs2_data(rs2_data)
-);
-
-// === Memory Unit ===
-logic [31:0] read_data;
-
-memory mem_unit (
-  .clk(clk),
-  .write_mem(mem_write),
-  .funct3(instruction[14:12]),
-  .write_address(alu_result),
-  .write_data(rs2_data),
-  .read_address(ra_mux_out),
-  .read_data(mem_read_data),
-  .led(),
-  .red(),
-  .green(),
-  .blue()
 );
 
 // === ALU ===
@@ -116,43 +102,52 @@ alu alu_unit (
   .alu_op(alu_op),
   .result(alu_result),
   .zero_flag(zero_flag)
-)
-// Mux's
-mux_2x1 pc_mux (
-    .in0(pc_plus_4),
-    .in1(alu_result),
-    .sel(jump),
-    .out(pc_next)
-)
+);
 
-mux_2x1 RA_mux (
-    .in0(pc),
-    .in1(alu_result),
-    .sel(jump),
-    .out(rdv_mux_out)
-)
+// === Memory Unit ===
+memory mem_unit (
+  .clk(clk),
+  .write_mem(mem_write),
+  .funct3(instruction[14:12]),
+  .write_address(alu_result),
+  .write_data(rs2_data),
+  .read_address(alu_result),
+  .read_data(mem_read_data),
+  .led(),
+  .red(),
+  .green(),
+  .blue()
+);
+
+// === Multiplexers ===
+mux_2x1 pc_mux (
+  .in0(pc_plus_4),
+  .in1(alu_result),
+  .sel(jump),
+  .out(pc_next)
+);
 
 mux_2x1 op1_mux (
-    .in0(pc),
-    .in1(rs1_data),
-    .sel(alu_src[0]),
-    .out(op1_mux_out)
-)
+  .in0(pc),
+  .in1(rs1_data),
+  .sel(alu_src[0]),
+  .out(op1_mux_out)
+);
 
 mux_2x1 op2_mux (
-    .in0(rs2_data),
-    .in1(imm_ext),
-    .sel(alu_src[0]),
-    .out(op2_mux_out)
-)
+  .in0(rs2_data),
+  .in1(imm_ext),
+  .sel(alu_src[1]),
+  .out(op2_mux_out)
+);
 
 mux_4x1 rdv_mux (
   .in0(imm_ext),
   .in1(alu_result),
-  .in2(pc_adder),
+  .in2(pc_plus_4),
   .in3(mem_read_data),
   .sel(mem_to_reg),
   .out(rdv_mux_out)
-)
+);
 
 endmodule
