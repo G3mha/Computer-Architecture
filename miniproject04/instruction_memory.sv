db294161764b97118bc4108f6ddcad34a901ced0
Enricco Gemha <enriccogemha@gmail.com>
2025-04-14 21:51:08 -0400

    refactor: Change filename for mem

--- miniproject04/instruction_memory.sv
+++ miniproject04/instruction_memory.sv
@@ -1,18 +1,18 @@
 // Instruction Memory Module
 module instruction_memory (
-    input  logic [31:0] address,
-    output logic [31:0] instruction
+  input  logic [31:0] address,
+  output logic [31:0] instruction
 );
 
-    // Memory array
-    logic [31:0] memory [0:255];
+  // Memory array
+  logic [31:0] memory [0:255];
 
-    // Load memory contents from file
-    initial begin
-        $readmemh("program.mem", memory);
-    end
+  // Load memory contents from file
+  initial begin
+    $readmemh("program.mem", memory);
+  end
 
-    // Word-aligned access (divide address by 4)
-    assign instruction = memory[address[31:2]];
+  // Word-aligned access (divide address by 4)
+  assign instruction = memory[address[31:2]];
 
 endmodule
